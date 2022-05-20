e014b9c3eff7c71ae3e01c45cbb2f05e8bbfc39f
Yana Kuzina <kuzina25.10.2013@gmail.com>
2022-05-20 18:49:45 +0300

    default props

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,12 +1,16 @@
 import PropTypes from "prop-types";
 
-const Title = ({ name, type = "normal" }) => {
+const Title = ({ name, type }) => {
   return <h1 style={{ fontWeight: type }}>Hello! {name}</h1>;
 };
 
 Title.propTypes = {
   name: PropTypes.string.isRequired,
   type: PropTypes.string,
+};
+
+Title.defaultProps = {
+  type: "normal",
 };
 
 export default Title;
